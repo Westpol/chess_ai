773eff5a15f7d1c073859b2402fda0288ea3dc32
Westpol <81779508+Westpol@users.noreply.github.com>
2023-02-17 08:20:18 +0100

    Added chess.update and Chess class

--- main.py
+++ main.py
@@ -5,20 +5,29 @@ screen = pygame.display.set_mode((800, 800))
 width, height = screen.get_size()
 
 
-def grid():
-    flip = True
-    for i in range(8):
-        flip = not flip
-        for k in range(8):
-            if flip:
-                pygame.draw.rect(screen, (125, 125, 125), (i * 100, k * 100, 100, 100))
-                flip = not flip
-            else:
-                pygame.draw.rect(screen, (0, 0, 0), (i * 100, k * 100, 100, 100))
-                flip = not flip
-    pygame.display.flip()
+class Chess:
+
+    def __init__(self):
+        pass
+
+    def grid(self):
+        flip = True
+        for i in range(8):
+            flip = not flip
+            for k in range(8):
+                if flip:
+                    pygame.draw.rect(screen, (125, 125, 125), (i * 100, k * 100, 100, 100))
+                    flip = not flip
+                else:
+                    pygame.draw.rect(screen, (0, 0, 0), (i * 100, k * 100, 100, 100))
+                    flip = not flip
+
+    def draw(self):
+        self.grid()
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
-    grid()
+    chess = Chess()
+    chess.draw()
     time.sleep(2)
